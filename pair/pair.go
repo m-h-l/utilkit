4b69e1cfaad681b05a3331688c9d5f4f2e89a2bb
m-h-l <54641248+m-h-l@users.noreply.github.com>
2024-04-10 09:15:42 +0000

    pair type

---
+++ pair/pair.go
@@ -0,0 +1,21 @@
+package pair
+
+type Pair[T any, U any] struct {
+	first  T
+	second U
+}
+
+func Of[T, U any](first T, second U) Pair[T, U] {
+	return Pair[T, U]{
+		first:  first,
+		second: second,
+	}
+}
+
+func (p Pair[T, U]) First() T {
+	return p.first
+}
+
+func (p Pair[T, U]) Second() U {
+	return p.second
+}
